02766ecb62067f1567a539792a66e24e0e7120dc
David Om <rahrahfez@hotmail.com>
2018-03-04 09:08:44 -0500

    updated methods/added Node class

--- src/Deque.java
+++ src/Deque.java
@@ -1,16 +1,20 @@
 import java.util.Iterator;
 
-public class Deque<Item> implements Iterable<Item> {
+public class Deque<Item> implements Iterable<Item> {    
     private class Node {
-	private Item item;
-	private Node next_;
-	private Node pevious_;
+	public Item item;
+	public Node next;
+	public Node pevious;
     }
     
     private int size_;
+    private Node first_;
+    private Node last_;
     
     public Deque() {
 	size_ = 0;
+	first_ = null;
+	last_ = null;
     }
     
     public boolean isEmpty() {
@@ -21,15 +25,34 @@ public class Deque<Item> implements Iterable<Item> {
 	return size_;
     }
     
-    public void addFirst(Item item) {}
-    public void addLast(Item item) {}
-    //public Item removeFirst();
-    //public Item removesLast();
+    public void addFirst(Item item) {
+	if (item == null)
+	    throw new java.lang.IllegalArgumentException();
+	
+	first_ = new Node();
+	Node temp = first_;
+	first_.item = item;
+	first_.next = temp;
+    }
+    
+    public void addLast(Item item) {
+	if (item == null)
+	    throw new java.lang.IllegalArgumentException();
+    }
+    public Item removeFirst() {
+	if (first_ == null)
+	    throw new java.util.NoSuchElementException();
+	Node temp = first_;
+	first_ = first_.previous;
+	first_ = null;
+    }
+    public Item removesLast() {}
     public Iterator<Item> iterator() {
-	return new DequeIterator();
+       return new DequeIterator();
     }
     private class DequeIterator implements Iterator<Item> {}
-  public static void main(String[] args) {
-      System.out.println("Deque.java");
-  }
+    
+    public static void main(String[] args) {
+       
+    }
 }
